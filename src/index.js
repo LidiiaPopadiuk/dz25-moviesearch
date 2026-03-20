7ea8c9d355dd83caf69c08739049e70790d3b608
LidiiaPopadiuk <lidiiadolfin@gmail.com>
2026-03-20 23:55:23 +0100

    fixing basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/dz25-moviesearch'>
+    <BrowserRouter basename='/dz25-moviesearch/main-page'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
