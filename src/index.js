fac5229e3ac3acb7c13c550e2077c22ba60024a7
LidiiaPopadiuk <lidiiadolfin@gmail.com>
2026-03-21 22:42:16 +0100

    replace BrowserRouter by HashRouter

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/dz25-moviesearch'>
+    <HashRouter basename='/dz25-moviesearch'>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
 
