b033dace6167c45e1e1f92067c4242c69d14b66b
LidiiaPopadiuk <lidiiadolfin@gmail.com>
2026-03-21 13:48:36 +0100

    change btn back

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/dz25-moviesearch/main-page'>
+    <BrowserRouter basename='/dz25-moviesearch'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
